3a660054d934b9468b47ff7283f6f82712ef3edd
Manuel Morini <morinimanuel@gmail.com>
2014-11-17 23:01:33 +0100

    starter example

---
+++ www/js/controllers/LoginCtrl.js
@@ -0,0 +1,34 @@
+(function(){
+
+    var LoginCtrl = function($scope,hello){
+    console.log("in login ctrl");
+        hello.init({
+            google : '656984324806-sr0q9vq78tlna4hvhlmcgp2bs2ut8uj8.apps.googleusercontent.com',
+            facebook : '160981280706879',
+            windows : '00000000400D8578'
+        },{
+          // Define the OAuth2 return URL
+          // This can be anything you like, providing its the callback which you have registered with the providers for OAuth2
+          // It could even be localhost, e.g. http://localhost/somepath as phonegap is not run from a domain so SameOrigin breaks,instead           // we take advantage of being able to read the popups URL in PhoneGap
+            redirect_uri : 'http://adodson.com/hello.js/redirect.html',
+            oauth_proxy: "https://auth-server.herokuapp.com/proxy"
+         });
+    
+        
+        $scope.loginHandler = function(r) {
+            logR(r);
+            hello(r.network).api('me').on('complete', log);
+        }
+        
+        $scope.logR = function(r){
+            var s = document.createTextNode(JSON.stringify(r, null, "\t"));
+            var pre = document.getElementsByTagName('pre')[0];
+            pre.insertBefore(s, pre.firstChild);
+        }
+        
+    };
+
+    LoginCtrl.$inject = ['$scope','hello'];
+
+    angular.module('starter').controller('LoginCtrl',LoginCtrl);
+}());
